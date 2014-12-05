e44f9a562ee58933b99dbd68cf3f2cbcb301c063
Renato Alves <renatodex@gmail.com>
2014-12-05 15:50:20 -0200

    Removed pry from require list (only development)

--- lib/intelipost-api.rb
+++ lib/intelipost-api.rb
@@ -1,4 +1,4 @@
-["pry", "logging", "httparty", "virtus", "colorize"].each { |lib| require lib }
+["logging", "httparty", "virtus", "colorize"].each { |lib| require lib }
 ['*.rb','models/*.rb', 'components/*.rb'].each do |p|
   Dir[File.dirname(__FILE__) + "/intelipost/api/#{p}"].each {|file| require file }
 end
